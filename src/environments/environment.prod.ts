69cee2ff6040111a1766a19ac0fad1615393bef4
jessiesalvador <jessiesalvador@users.noreply.github.com>
2026-05-09 19:32:37 +0000

    deploy: 263b6b3a38b545da17b6d7f55e1225def65ed823

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  propTrackApiKey: 'YOUR_PROPTRACK_API_KEY_HERE'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  propTrackApiKey: 'YOUR_PROPTRACK_API_KEY_HERE'
-};
